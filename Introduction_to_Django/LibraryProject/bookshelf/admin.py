ef0a61f389b586ec12c4587da0b3d00348c2b4b9
Michael Darko <mykdarkoxyz@gmail.com>
2025-07-11 12:41:11 +0000

    admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,3 +3,9 @@ from .models import Book
 # Register your models here.
 admin.site.register(Book)
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+    ordering = ('title',)
+    date_hierarchy = 'publication_year'
